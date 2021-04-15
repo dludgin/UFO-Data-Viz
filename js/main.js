87aafe760b15bcefc3f812c9a065e68a75bf685f
dludgin <70551731+dludgin@users.noreply.github.com>
2021-04-14 22:13:28 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -83,6 +83,8 @@ var margin = {top: 20, right: 30, bottom: 50, left: 20},
         // .attr("transform", "translate(" + 10 + "," + 0 + ")")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
+    	.attr("viewBox", "0 0 " + width + " " + height)
+    	.attr("preserveAspectRatio","xMidYMid meet")
         .append('g')
         .attr("transform", 
              "translate(" + margin.left + "," + margin.bottom + ")");
